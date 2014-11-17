844651a50b94b800d7b14a6defa53173ecef77da
zhiyb <yz39g13@soton.ac.uk>
2014-11-17 03:00:18 +0000

    P3 Small change

--- P3/Complex/main.cpp
+++ P3/Complex/main.cpp
@@ -64,15 +64,9 @@ class Complex psi(double t, double x)
 #define PLOT_STEP	0.001
 int main(void)
 {
-	Sleep(1000);		// Get ride of a wired warning message
+	Sleep(100);		// Get ride of a wired warning message
 #if 0
 	//test();
-	double x;
-	cout << "Enter x value for evaluate:" << endl;
-	cin >> x;
-	cout << endl << "t = 0:  \t" << psi(0, x) << endl;
-	cout << "t = 0.5:\t" << psi(0.5, x) << endl;
-	cout << "t = 1:  \t" << psi(1, x) << endl << endl;
 
 	double sum = 0;
 	for (double x = -L; x <= L; x += 2.0 * L / N) {
@@ -83,8 +77,15 @@ int main(void)
 			sum += (res * res.conj()).real();
 	}
 	sum *= 2.0 * L / N;
-	cout << "Integration of psi * psi.conj() from " << -L << " to " << L << " with " << N << " steps:" << endl;
+	cout << "Integration of psi * psi.conj() (t = 0) from " << -L << " to " << L << " with " << N << " steps:" << endl;
 	cout << sum << endl;
+
+	double x;
+	cout << "Enter x value for evaluate:" << endl;
+	cin >> x;
+	cout << endl << "t = 0:  \t" << psi(0, x) << endl;
+	cout << "t = 0.5:\t" << psi(0.5, x) << endl;
+	cout << "t = 1:  \t" << psi(1, x) << endl << endl;
 #else
 	fstream data(PLOT_FILE ".dat", fstream::out), m(PLOT_FILE ".m", fstream::out);
 	if (!data.is_open() || !m.is_open()) {
@@ -94,13 +95,13 @@ int main(void)
 	for (double x = PLOT_MIN; x <= PLOT_MAX; x += PLOT_STEP)
 		data << x << '\t' << psi(0, x).real() << '\t' << psi(0.5, x).real() << '\t' << psi(1, x).real() << endl;
 	m << "load " PLOT_FILE ".dat;" << endl;
-	m << "t = " PLOT_FILE "(:, 1);" << endl;
+	m << "x = " PLOT_FILE "(:, 1);" << endl;
 	m << "psi0 = " PLOT_FILE "(:, 2);" << endl;
 	m << "psi0_5 = " PLOT_FILE "(:, 3);" << endl;
 	m << "psi1 = " PLOT_FILE "(:, 4);" << endl;
 	m << "hold off;" << endl;
 	m << "figure(1);" << endl;
-	m << "plot(t, psi0, 'b-', t, psi0_5, 'r-', t, psi1, 'g-');" << endl;
+	m << "plot(x, psi0, 'b-', x, psi0_5, 'r-', x, psi1, 'g-');" << endl;
 	m << "ylabel('amplitude of the real part of \\psi');" << endl;
 	m << "xlabel('t=0 (blue), t=1/2s (red), t=1s (green)');" << endl;
 	m << "title('electron wave packet evolution');" << endl;
