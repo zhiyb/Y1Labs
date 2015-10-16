1545808d58b1bd04778381895076368e9c9ed84b
Norman Zhi <normanzyb@gmail.com>
2015-10-16 01:48:58 +0100

    D2 Code indentation

--- D2/CPU/pc.sv
+++ D2/CPU/pc.sv
@@ -6,7 +6,7 @@
 // Description: Program counter for basic processor
 //            :
 // Limitations: None
-//            : 
+//            :
 // System     : SystemVerilog IEEE 1800-2005
 //            :
 // Author     : Mark Zwolinski
@@ -20,9 +20,9 @@
 /////////////////////////////////////////////////////////////////////
 
 module PC #(parameter WORD_W = 8, OP_W = 3)
-               (input logic clock, n_reset, PC_bus, load_PC, INC_PC, 
+               (input logic clock, n_reset, PC_bus, load_PC, INC_PC,
                 inout wire [WORD_W-1:0] sysbus);
-		
+
 logic [WORD_W-OP_W-1:0] count;
 
 assign sysbus = PC_bus ? {{OP_W{1'b0}},count} : {WORD_W{1'bZ}};
@@ -36,6 +36,6 @@ always_ff @(posedge clock, negedge n_reset)
       if (INC_PC)
         count <= count + 1;
       else
-	      count <= sysbus;
+        count <= sysbus;
   end
 endmodule
